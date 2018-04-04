c3582aaaf297f2727adf0a123a9425c1b8b61fb5
yw <myoueva@gmail.com>
2018-04-04 11:41:47 +0800

    项目目录初始化

---
+++ restshop/apps/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @Time    : 2018/4/4 上午11:17
+# @Author  : YouMing
+# @Email   : myoueva@gmail.com
+# @File    : __init__.py.py
+# @Software: PyCharm
+# @license : 娱网科道信息技术有限公司 copyright © 2015-2016
