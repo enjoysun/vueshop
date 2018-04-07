7ecdf09d599f042dce76c40cc62636c175997a78
mypc <1570819807@qq.com>
2018-04-07 23:47:43 +0800

    单文件操作model，添加类别数据

---
+++ restshop/db_tools/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+# encoding: utf-8
+
+# @author: myou
+
+# @license: (C) Copyright 2013-2017, Node Supply Chain Manager Corporation Limited.
+
+# @contact: myoueva@gmail.com
+
+# @software: garner
+
+# file: __init__.py
+
+# @time: 2018/4/7 23:34
---
+++ restshop/db_tools/cateimport.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#@author: myou
+
+#@license: (C) Copyright 2013-2017, Node Supply Chain Manager Corporation Limited.
+
+#@contact: myoueva@gmail.com
+
+#@software: garner
+
+#@file: cateimport.py
+
+#@time: 2018/4/7 22:19
