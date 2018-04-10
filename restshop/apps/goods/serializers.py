09a18e69f4e2331eccf62c7ffe7f4f19dc538380
mypc <1570819807@qq.com>
2018-04-10 23:55:01 +0800

    modelserailizer学习

--- restshop/apps/goods/serializers.py
+++ restshop/apps/goods/serializers.py
@@ -9,13 +9,20 @@
 # @license : 娱网科道信息技术有限公司 copyright © 2015-2016
 
 from rest_framework import serializers
+from .models import Goods, GoodCategory
 
 
-class Goodcateserializer(serializers.Serializer):
-    CATEGORY_TYPE = (
-        (1, '一级类目'),
-        (2, '二级类目'),
-        (3, '三级类目'),
-    )
-    name = serializers.CharField(max_length=32, required=True)
-    code = serializers.CharField(max_length=32)
+
+
+class Goodcateserializer(serializers.ModelSerializer):
+    # CATEGORY_TYPE = (
+    #     (1, '一级类目'),
+    #     (2, '二级类目'),
+    #     (3, '三级类目'),
+    # )
+    # name = serializers.CharField(max_length=32, required=True)
+    # code = serializers.CharField(max_length=32)
+    class Meta:
+        model = Goods()
+        # fields = '__all__'
+        fields = ['category', 'name']
