90ce57705fb84dc37b2d617099e7d482543282ab
yw <myoueva@gmail.com>
2018-04-11 14:48:22 +0800

    解决包导入相对路径，包内文件不能被作为脚本执行问题

--- restshop/db_tools/data/__init__.py
+++ restshop/db_tools/data/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# @Time    : 2018/4/11 下午2:13
+# @Time    : 2018/4/11 下午2:12
 # @Author  : YouMing
 # @Email   : myoueva@gmail.com
 # @File    : __init__.py.py
